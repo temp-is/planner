3f85cc2d2682746456b2ba2f2cb0036785e307f9
Haim Yannai <HaimYa@imc-grp.com>
2023-06-06 11:54:13 +0300

    header by haim task 0001

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatMenuModule } from '@angular/material/menu';
 
-
-
-const modules: Array<any> = [MatToolbarModule, MatIconModule, MatButtonModule]
+const modules: Array<any> = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTreeModule,
+  MatMenuModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ...modules
-  ],
-  exports: modules
+  imports: [CommonModule, ...modules],
+  exports: modules,
 })
-export class MaterialModule { }
+export class MaterialModule {}
