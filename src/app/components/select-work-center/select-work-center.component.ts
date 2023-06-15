9310c3126636214d78737c13babbf2640b1aa76b
RafiLevi81 <Refaell@iscar.co.il>
2023-06-15 10:30:20 +0300

    fix services call

--- src/app/components/select-work-center/select-work-center.component.ts
+++ src/app/components/select-work-center/select-work-center.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GlobalService } from 'src/app/services/global.service';
-import {
-  Factory,
-  IMachine,
-  Machine,
-  WorkCenter,
-  WorkCenterType,
-} from 'src/app/shared/models';
+import { IFactory, IMachine, IWorkCenter } from 'src/app/shared/models';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { FormControl, NgForm } from '@angular/forms';
 import { StorageService } from 'src/app/services/storage.service';
+import { DEFAULT_DIALOG_CONFIG } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-select-work-center',
@@ -18,15 +13,9 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./select-work-center.component.scss'],
 })
 export class SelectWorkCenterComponent {
-  public factoriesArray: Array<Factory> = [];
-  public workCentersArray: Array<WorkCenter> = [];
-  public WorkCenterTypeArray: Array<WorkCenterType> = [];
+  public factoriesArray: Array<IFactory> = [];
+  public workCentersArray: Array<IWorkCenter> = [];
   public machineArray: Array<IMachine> = [];
-
-  inputValue: number = 0;
-  counter: number = 0;
-  maxCounter: number = 100;
-  minCounter: number = 0;
 
   includeOfficeCheckBox: boolean = false;
   viewModeCheckBox: boolean = false;
@@ -38,41 +27,25 @@ export class SelectWorkCenterComponent {
     private storage: StorageService
   ) {}
 
-  selectedFactory = 'option2';
-  selectedWorkCenterType = 'option2';
-  selectedWorkCenter = 'option2';
+  numbers: number[] = Array.from({ length: 20 }, (_, i) => i + 1);
 
   onFactoryChange(factoryId: string): void {
-    debugger;
+    this.globalService.getWorkCenter().subscribe((data) => {
+      this.workCentersArray = data;
+    });
   }
-  ngOnInit() {
-    this.showComponent = true;
-    this.factoriesArray = this.storage.getData('factorylist');
-
-    this.globalService.getWorkCenter().subscribe((data) => {
-      this.WorkCenterTypeArray = data.type;
-      this.workCentersArray = data.wc;
-    });
-    this.globalService.getWorkCenter().subscribe((data) => {
-      this.WorkCenterTypeArray = data.type;
-      this.workCentersArray = data.wc;
-    });
+  onWorkCenterChange(workCenterId: string): void {
     this.globalService.getMachine().subscribe((data) => {
       this.machineArray = data;
     });
   }
-  public incrementCounter() {
-    if (this.counter < this.maxCounter) {
-      this.counter++;
-    }
+
+  ngOnInit() {
+    this.showComponent = true;
+    this.factoriesArray = this.storage.getData('factorylist');
   }
-  public decrementCounter() {
-    if (this.counter > this.minCounter) {
-      this.counter--;
-    }
-  }
+
   public onSubmit(loginForm: NgForm): void {
-    debugger;
     this.showComponent = false;
     this.globalService.createWorkcenterData(loginForm.value);
   }
--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -2,13 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin, map, of } from 'rxjs';
 import { Observable } from 'rxjs';
-import {
-  Factory,
-  IMachine,
-  UserDetails,
-  WorkCenter,
-  WorkCenterList,
-} from '../shared/models';
+import { IMachine, IUnloadedOrders, IWorkCenter } from '../shared/models';
 import { API } from '../core/API';
 
 @Injectable({
@@ -17,117 +11,18 @@ import { API } from '../core/API';
 export class GlobalService {
   constructor(private http: HttpClient) {}
 
-  public getCompanies(): Observable<string[]> {
-    const url: string = 'https://someDomain.com/iscar'; // ["company 1", "company 2"]
-    const mockData = ['company 1', 'company 2', 'company 3'];
-
-    return of(mockData);
-
-    return <Observable<string[]>>this.http.get(url).pipe(
-      map((data) => {
-        // return data;
-
-        return ['company 1', 'company 2', 'company 3'];
-      })
-    );
-  }
-
-  public getFactories(): Observable<Factory[]> {
-    const url: string = 'https://someDomain.com/iscar'; // ["company 1", "company 2"]
-    const factoryDetails: Factory[] = [
-      {
-        Code: 'GA',
-        Name: 'GA: General & Admin',
-      },
-      {
-        Code: 'HA',
-        Name: 'HA: Carbide A',
-      },
-      {
-        Code: 'XX',
-        Name: 'XX: Powder,for CMS',
-      },
-    ];
-
-    return of(factoryDetails);
-  }
-
-  public getWorkCenter(): Observable<WorkCenterList> {
-    const url: string = 'https://someDomain.com/iscar'; // ["company 1", "company 2"]
-    const workCenterList: WorkCenterList = {
-      type: [
-        {
-          TpCode: 'DR',
-          TpName: 'DR : DRILLING',
-        },
-        {
-          TpCode: 'CR',
-          TpName: 'CR : TURNING',
-        },
-      ],
-      wc: [
-        {
-          Code: '22',
-          Desc: 'M1-TURN MANUAL',
-          DescLocal: 'M1-TOURNAGE MAN',
-          Name: '22 : M1-TURN MANUAL',
-          TpCode: 'CR',
-          checkSOP: false,
-          includeOffOpr: false,
-          isBatch: false,
-          nonWorkingDays: [0, 6],
-          numOfOprBfr: 25,
-        },
-      ],
-    };
-
-    return of(workCenterList);
+  public getWorkCenter(): Observable<IWorkCenter[]> {
+    return this.http.get<IWorkCenter[]>(API['workcenterlist']);
   }
 
   public getMachine(): Observable<IMachine[]> {
-    const url: string = 'https://someDomain.com/iscar';
-    const machine: IMachine[] = [
-      {
-        Name: '03: TBT ML200-2-800',
-        desc: 'TBT ML200-2-800',
-        machId: '03',
-      },
-      {
-        Name: '04: TBT ML200-2-800',
-        desc: 'TBT ML200-2-800',
-        machId: '04',
-      },
-      {
-        Name: '05: TBT ML200-2-800',
-        desc: 'TBT ML200-2-800',
-        machId: '05',
-      },
-      {
-        Name: '06: TBT ML200-2-800',
-        desc: 'TBT ML200-2-800',
-        machId: '06',
-      },
-    ];
-
-    return of(machine);
+    return this.http.get<IMachine[]>(API['machielist']);
   }
-
-  public getUserDetails(): Observable<UserDetails> {
-    //Need to understand how to get all of the data
-    const url: string = 'https://someDomain.com/iscar';
-    const userDetails: UserDetails = {
-      CanWrite: true,
-      extjsUrl: 'https://extjs-ltd.ssl.imc-grp.com',
-      UserStat: true,
-      username: 'ISYAH',
-      company: 'XZ',
-      company_name: 'TEST COMPANY',
-    };
-    return of(userDetails);
+  public getunloadedorders(): Observable<IUnloadedOrders[]> {
+    return this.http.get<IUnloadedOrders[]>(API['unloadedOrders']);
   }
 
   public createWorkcenterData(data: any): Observable<{ [key: string]: any }> {
-    debugger;
     return forkJoin({
       //resourceStore: this.http.get(API['getresourcestore']),
       // availability: this.http.get(API['getavailability']),
